1b2d8b7bbd0edf55728062712915b0e6e2325bfc
zXinGChenG <116803160+zXinGChenG@users.noreply.github.com>
2024-04-01 01:23:03 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
+import "@/assets/global.css"
+
+Vue.config.productionTip = false
+Vue.use(ElementUI, { size: "small" });
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
